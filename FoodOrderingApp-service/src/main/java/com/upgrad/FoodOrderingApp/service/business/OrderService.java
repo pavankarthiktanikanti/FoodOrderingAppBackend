11e630941ca6764eef76c17e648da6b064be300f
pavankarthiktanikanti <pavankarthiktanikanti@gmail.com>
2020-02-25 18:36:47 +0530

    Add separate DAO for Coupon Entity #18
    - Move coupon by coupon name to CouponDao

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/business/OrderService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/business/OrderService.java
@@ -1,5 +1,6 @@
 package com.upgrad.FoodOrderingApp.service.business;
 
+import com.upgrad.FoodOrderingApp.service.dao.CouponDao;
 import com.upgrad.FoodOrderingApp.service.dao.OrderDao;
 import com.upgrad.FoodOrderingApp.service.entity.CouponEntity;
 import com.upgrad.FoodOrderingApp.service.exception.CouponNotFoundException;
@@ -11,10 +12,14 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     @Autowired
+    private CouponDao couponDao;
+
+    @Autowired
     private OrderDao orderDao;
 
     /**
      * Retrieve the Coupon Information matched with the Coupon name passed
+     *
      * @param couponName The coupon name for which the coupon details has to be retrieved
      * @return The Coupon info matched with the coupon name
      * @throws CouponNotFoundException If the coupon name doesn't match with the Database records
@@ -24,7 +29,7 @@ public class OrderService {
         if (FoodOrderingUtil.isInValid(couponName)) {
             throw new CouponNotFoundException("CPF-002", "Coupon name field should not be empty");
         }
-        CouponEntity coupon = orderDao.getCouponByCouponName(couponName);
+        CouponEntity coupon = couponDao.getCouponByCouponName(couponName);
         // No match with the Database for the coupon name
         if (coupon == null) {
             throw new CouponNotFoundException("CPF-001", "No coupon by this name");
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/OrderDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/OrderDao.java
@@ -13,24 +13,4 @@ public class OrderDao {
 
     @PersistenceContext
     private EntityManager entityManager;
-
-    /**
-     * Retrieves the Coupon Entity matched with the coupon name
-     *
-     * @param couponName The coupon name to be searched in Database
-     * @return The Coupon Entity matched with the coupon name
-     */
-    public CouponEntity getCouponByCouponName(String couponName) {
-        try {
-            // Reading as List, if there are several records matching with the same coupon, take the first coupon record
-            List<CouponEntity> couponList = entityManager.createNamedQuery("couponByCouponName", CouponEntity.class).setParameter("couponName", couponName).getResultList();
-            if (couponList != null && !couponList.isEmpty()) {
-                return couponList.get(0);
-            } else {
-                return null;
-            }
-        } catch (NoResultException e) {
-            return null;
-        }
-    }
 }
