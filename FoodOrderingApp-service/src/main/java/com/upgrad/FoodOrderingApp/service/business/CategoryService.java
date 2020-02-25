45d89a48f71a0a35b0ece3be9b7ad8f8f180b555
pavankarthiktanikanti <pavankarthiktanikanti@gmail.com>
2020-02-25 10:45:45 +0530

    Add Get All Restaurants Feature #12
    - Add mapping /restaurant to view the list of available restaurant
    
    - Add new Restaurant Entity for RESTAURANT table
    
    - Add new Restaurant Category Entity for RESTAURANT_CATEGORY table
    
    - Sort the restaurants order by ratings descending

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/business/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/business/CategoryService.java
@@ -1,10 +1,13 @@
 package com.upgrad.FoodOrderingApp.service.business;
 
 import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
+import com.upgrad.FoodOrderingApp.service.dao.RestaurantCategoryDao;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantCategoryEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,6 +16,9 @@ public class CategoryService {
     @Autowired
     private CategoryDao categoryDao;
 
+    @Autowired
+    private RestaurantCategoryDao restaurantCategoryDao;
+
     /**
      * Retrieves the List of Categories from the Database order by name
      *
@@ -20,5 +26,22 @@ public class CategoryService {
      */
     public List<CategoryEntity> getAllCategoriesOrderedByName() {
         return categoryDao.getAllCategoriesOrderedByName();
+    }
+
+    /**
+     * This method fetches the list of all categories based on the restaurant uuid
+     *
+     * @param restaurantUUID The uuid of the restaurant to search for categories in Database
+     * @return The list of categories matched with the restaurant uuid
+     */
+    public List<CategoryEntity> getCategoriesByRestaurant(String restaurantUUID) {
+        List<RestaurantCategoryEntity> restaurantCategories = restaurantCategoryDao.getRestaurantCategoriesByRestaurantUUID(restaurantUUID);
+        List<CategoryEntity> categories = new ArrayList<CategoryEntity>();
+        if (restaurantCategories != null) {
+            restaurantCategories.forEach(restaurantCategory -> {
+                categories.add(restaurantCategory.getCategory());
+            });
+        }
+        return categories;
     }
 }
